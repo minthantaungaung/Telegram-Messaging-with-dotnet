28f3ff049f17a9b1cab977c0e05202f39b039bdd
MINTHANT <minthant31265809@gmail.com>
2022-08-10 15:10:09 +0630

    .

---
+++ Bot/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Bot.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string sendMsg(string msg = "")
+        {
+            string urlString = "https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}";
+            string apiToken = "1492705751:AAF6wtnKtLUPkbA2IQ5aQ9v38FJpe5LSOmY";
+            string chatId = "-1001509986398";
+            string text = msg;
+            urlString = string.Format(urlString, apiToken, chatId, text);
+            WebRequest request = WebRequest.Create(urlString);
+            Stream rs = request.GetResponse().GetResponseStream();
+            StreamReader reader = new StreamReader(rs);
+            string line = "";
+            StringBuilder sb = new StringBuilder();
+            while (line != null)
+            {
+                line = reader.ReadLine();
+                if (line != null)
+                    sb.Append(line);
+            }
+            string response = sb.ToString();
+           
+            return (response);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
